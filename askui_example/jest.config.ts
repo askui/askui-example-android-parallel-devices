125ee755927e8f7d7fed1861e8fbf8bc86cb0cd0
Samir mlika <105347215+mlikasam-askui@users.noreply.github.com>
2024-02-23 11:15:08 +0100

    Fix Jest Config

--- askui_example/jest.config.ts
+++ askui_example/jest.config.ts
@@ -8,6 +8,7 @@ const config: Config.InitialOptions = {
   sandboxInjectedGlobals: [
     'Math',
   ],
+  maxWorkers: process.env.JEST_MAX_WORKER ? parseInt(process.env.JEST_MAX_WORKER) : 1
 };
 
 // eslint-disable-next-line import/no-default-export
